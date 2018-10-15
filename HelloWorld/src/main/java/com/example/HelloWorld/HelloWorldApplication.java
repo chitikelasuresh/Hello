2bad68f06936d3b6ab45b06fcb8de63bf50d10cc
EAD\schitikela2 <Munda@9679>
2018-10-15 11:43:13 +0530

    first commit

---
+++ HelloWorld/src/main/java/com/example/HelloWorld/HelloWorldApplication.java
@@ -0,0 +1,20 @@
+package com.example.HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+	
+	@GetMapping("/")
+	public String getMessage() {
+		return "hello Suresh";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
